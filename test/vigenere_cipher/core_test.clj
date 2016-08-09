7d64168be3901aa6f62c038cf3f66fa83d20d4e0
Gaivile <gaivile.info3@gmail.com>
2016-08-09 01:20:50 +0100

    Create core_test.clj

---
+++ test/vigenere_cipher/core_test.clj
@@ -0,0 +1,7 @@
+(ns vigenere-cipher.core-test
+  (:require [clojure.test :refer :all]
+            [vigenere-cipher.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
